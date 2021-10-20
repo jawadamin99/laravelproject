068c88a84282658882838dfdf826daa8d97f81c4
JawadAmin <info@jawadamin.com>
2021-10-21 01:50:21 +0500

    insert user

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Customer;
+use \App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
 
 class HomeController extends Controller
 {
@@ -31,15 +31,18 @@ class HomeController extends Controller
     }
     public function register_handler(Request $request)
     {
-        $validator = Validator::make($request->all(),[
-            'firstName'=>'required|max:255',
-            'lastName'=>'required|max:255',
-            'emailAddress'=>'required|customers:UserEmail,email|max:255',
-            'password'=>'required|max:255'
-        ])->validate();
-        $validated = $validator->validated();
-        dd($validator);
-        Customer::create($validated);
-        dd(Customer::all());
+        $request->validate([
+           'firstName'=>'required|max:255',
+           'lastName'=>'required|max:255',
+           'emailAddress'=>'required|unique:users,email|max:255',
+           'password'=>'required|max:255'
+        ]);
+        $insert_data = [
+            'name'=>$request->firstName,
+            'email'=>$request->emailAddress,
+            'password'=>Hash::make($request->password),
+        ];
+        User::create($insert_data);
+        dd(User::all());
     }
 }
